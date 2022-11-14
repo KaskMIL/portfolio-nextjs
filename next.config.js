ba02c235a570043e735cbd647da1f9796fbb596b
toMilanesi <tomi.milanesi@gmail.com>
2022-11-14 18:58:50 -0300

    Install scrollbar, add global styles

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+      port: '',
+      pathname: '/dgpvrqyby/**'
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
