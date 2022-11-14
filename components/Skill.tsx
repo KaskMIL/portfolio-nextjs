ec8dfba02b372f9f90511a9415a3a217ba5600a5
toMilanesi <tomi.milanesi@gmail.com>
2022-11-14 18:58:18 -0300

    Refactor hover and sizes

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import react from './assets/icons8-react-native-240.png';
-import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 type Props = {
   directionLeft?: boolean;
+  image: string;
+  name: string
 }
 
-const Skill = ({ directionLeft }: Props) => {
+const Skill = ({ directionLeft, image, name }: Props) => {
   return (
     <motion.div
       initial={{
@@ -23,14 +23,13 @@ const Skill = ({ directionLeft }: Props) => {
       }}
       className='relative cursor-pointer flex'
     >
-      <Image
-        src={react}
-        alt='react logo'
-        className='border border-gray-500 rounded-full h-12 w-12 md:h-24 md:w-24 p-2 hover:scale-110 transition duration-500 ease-in-out'
+      <img
+        src={image} alt="react logo"
+        className='border border-gray-500 rounded-full h-12 w-12 md:h-24 md:w-24 p-2 hover:scale-110 transition duration-500 ease-in-out object-fill'
       />
-      <div className='absolute opacity-0 hover:opacity-100 transition duration-500 ease-in-out bg-white w-12 h-12 md:h-24 md:w-24 rounded-full'>
+      <div className='absolute opacity-0 transition duration-500 ease-in-out bg-[#F7AB0A] hover:opacity-90 w-12 h-12 md:h-24 md:w-24 rounded-full'>
         <div className='flex justify-center items-center h-full'>
-          <p className='md:text-2xl text-gray-500'>100%</p>
+          <p className='md:text-xl text-[9px] text-gray-500'>{name}</p>
         </div>
       </div>
     </motion.div>
