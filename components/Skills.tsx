54726b8f1f2c4883811f19ace2cca11d04fe5f5b
toMilanesi <tomi.milanesi@gmail.com>
2022-11-14 10:04:53 -0300

    Add Skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { motion } from 'framer-motion';
+import Skill from './Skill';
 
 type Props = {}
 
 const Skills = (props: Props) => {
+  const directionLeft = true;
   return (
     <motion.div
       initial={{ opacity: 0}}
       whileInView={{ opacity: 1}}
-      transition={{ duration: 5 }}
-      className='h-screen relative flex flex-col items-center text-center md:text-left xl:flex-row justify-center'
+      transition={{ duration: 3 }}
+      className='h-screen relative flex flex-col items-center text-center md:text-left xl:flex-row justify-center p-5'
     >
       <h3 className='absolute top-24 uppercase tracking-[15px] font-semibold text-gray-500 text-2xl'>Skills</h3>
-      <h3 className='absolute top-[134px] uppercase tracking-[3px] font-light text-gray-500 text-lg'>Hover over a skill</h3>
+      <h3 className='absolute top-[134px] uppercase tracking-[3px] font-light text-gray-500 text-sm'>Hover over a skill</h3>
 
-      <div className='grid grid-cols-4 space-x-5 justify-center'>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-
+      <div className='grid grid-cols-4 gap-5 bg-[#292929] p-5 rounded-lg shadow-sm shadow-stone-900'>
+        <Skill />
+        <Skill directionLeft={true} />
+        <Skill />
+        <Skill directionLeft={true} />
+        <Skill />
+        <Skill directionLeft={true} />
+        <Skill />
+        <Skill directionLeft={true} />
       </div>
     </motion.div>
   )
