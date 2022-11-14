e3bed6050f94066e29d0471803970439d8bae5e5
toMilanesi <tomi.milanesi@gmail.com>
2022-11-14 11:24:37 -0300

    Add Projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 
 export default function Home() {
@@ -21,22 +22,22 @@ export default function Home() {
         <Hero />
       </section>
 
-      {/* About */}
       <section id='about' className='snap-center'>
         <About />
       </section>
 
-      {/* Experience */}
       <section id='experiences' className='snap-center'>
         <WorkExperience />
       </section>
 
-      {/* Skills */}
       <section id='skills' className='snap-center'>
         <Skills />
       </section>
 
       {/* Projects */}
+      <section id='projects' className='snap-center'>
+        <Projects />
+      </section>
 
       {/* Contact */}
     </div>
