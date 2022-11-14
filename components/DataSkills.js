0e2ff9e1796895d2abcf81d43727384b3f6be8d7
toMilanesi <tomi.milanesi@gmail.com>
2022-11-14 18:56:54 -0300

    Add data container with name and images url

---
+++ components/DataSkills.js
@@ -0,0 +1,54 @@
+const languagesSkills = [
+  {
+    name: 'JavaScript',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668456804/icons8-javascript-240_dh3mxr.png',
+  },
+  {
+    name: 'Ruby',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668458752/icons8-ruby-programming-language-240_hj2dhk.png',
+  },
+  {
+    name: 'CSS3',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668456804/icons8-css3-240_k2y8hw.png',
+  },
+  {
+    name: 'HTML5',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668456804/icons8-html-5-240_rtyw46.png',
+  },
+]
+
+const frameworksSkills = [
+  {
+    name: 'React',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668456804/icons8-react-native-240_zcxevt.png',
+  },
+  {
+    name: 'Redux',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668460078/icons8-redux-240_kt9bta.png',
+  },
+  {
+    name: 'Tailwind',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668458752/icons8-tailwind-css-240_onffb4.png',
+  },
+  {
+    name: 'Bootstrap',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668458752/icons8-bootstrap-240_gyhllc.png',
+  },
+]
+
+const toolsSkills = [
+  {
+    name: 'GIT',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668458752/icons8-git-240_gsf4mi.png',
+  },
+  {
+    name: 'GitHub',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668458752/icons8-github-240_j71tvu.png',
+  },
+  {
+    name: 'Terminal',
+    url: 'https://res.cloudinary.com/dgpvrqyby/image/upload/v1668459990/icons8-console-100_casprk.png'
+  }
+]
+
+export { languagesSkills, frameworksSkills, toolsSkills };
