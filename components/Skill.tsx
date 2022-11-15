75a77a5560122a7c5041daa7d52ef95799426de8
toMilanesi <tomi.milanesi@gmail.com>
2022-11-15 07:33:23 -0300

    refactor to Image component

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 type Props = {
@@ -23,10 +24,17 @@ const Skill = ({ directionLeft, image, name }: Props) => {
       }}
       className='relative cursor-pointer flex'
     >
-      <img
-        src={image} alt="react logo"
+      <Image
+        src={image}
+        alt='tech'
+        width='50'
+        height='50'
         className='border border-gray-500 rounded-full h-12 w-12 md:h-24 md:w-24 p-2 hover:scale-110 transition duration-500 ease-in-out object-fill'
       />
+      {/* <img
+        src={image} alt="react logo"
+        className='border border-gray-500 rounded-full h-12 w-12 md:h-24 md:w-24 p-2 hover:scale-110 transition duration-500 ease-in-out object-fill'
+      /> */}
       <div className='absolute opacity-0 transition duration-500 ease-in-out bg-[#F7AB0A] hover:opacity-90 w-12 h-12 md:h-24 md:w-24 rounded-full'>
         <div className='flex justify-center items-center h-full'>
           <p className='md:text-xl text-[9px] text-gray-500'>{name}</p>
