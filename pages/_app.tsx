403333f97c8258eff2dc74be32af62c4d33277f1
toMilanesi <tomi.milanesi@gmail.com>
2022-11-16 11:38:42 -0300

    Add google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-C57GT3EVP5" />
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-C57GT3EVP5', {
+            page_path: window.location.pathname,
+          });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+    )
 }
